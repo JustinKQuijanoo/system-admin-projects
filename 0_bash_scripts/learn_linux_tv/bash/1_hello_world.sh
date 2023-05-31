0a602733349bbf41d3eb6584304fcc5a2f931185
root <root@HM01.4d2izpb3fsnedb3t0wyllqbefb.cx.internal.cloudapp.net>
2023-05-31 05:36:02 +0000

    removed files, updated folders

--- 0_bash_scripts/learn_linux_tv/bash/1_hello_world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The very first line indicates that this script uses the bash interpreter
-
-# Use the 'echo' command to print stuff to the terminal
-echo "Hello World"
-
-echo "Current working directory: "
-pwd
--- 0_bash_scripts/learn_linux_tv/bash/6.1_while_loops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while [ -f ~/testfile ]
-do
-    echo "As of $(date), this file exists."
-    sleep 0.10
-done
-
-echo "As of $(date), this file no longer exists."
